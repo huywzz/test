e9041070443269b2232f15c950ca07ce1e66e6eb
huywzz <93022280+huywzz@users.noreply.github.com>
2021-12-12 11:52:54 +0700

    first

---
+++ Bai2/src/Bai2.java
@@ -0,0 +1,17 @@
+import java.nio.file.FileSystemLoopException;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a=3;
+	int b=6,c=0;
+	c=a;
+	a=b;
+	b=c;
+	System.out.println(a);
+	System.out.println(b);
+
+	}
+	
+}
---
+++ Bai2/src/test.java
@@ -0,0 +1,24 @@
+
+public class test {
+
+	public class function_chuoi {
+		static int count ( String str, String word) {
+			String array[] = str.split(" ");
+			int dem = 0;
+			for (int i=0; i<=str.length(); i++) {
+				if(word.equals(array[i])) { 
+					dem++;}
+			}
+			return dem;
+		}
+
+		public static void main(String[] args) {
+			String str="Java is exciting. I love java";
+			String word="java";
+			System.out.println(count(str, word));
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
